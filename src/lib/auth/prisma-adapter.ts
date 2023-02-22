1fa5dfea3c1303d752062128506cabfa504c1be1
Pedro Henrique Fernandes <pedro007henrique41@gmail.com>
2023-02-22 14:05:10 -0300

    Fix: bug in username in create user

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -21,7 +21,6 @@ export function PrismaAdapter(
         },
         data: {
           name: user.name,
-          username: user.username,
           email: user.email,
           avatar_url: user.avatar_url,
         },
@@ -117,7 +116,6 @@ export function PrismaAdapter(
         },
         data: {
           name: user.name,
-          username: user.username,
           email: user.email,
           avatar_url: user.avatar_url,
         },
